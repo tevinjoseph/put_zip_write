8fb3256d67d6e5166cdc9c4bca5dc4c4859762e4
Tevin Joseph <tevin@nuventure.in>
2016-03-31 15:53:53 +0530

    Initail commit - Accepts a zip file in put request and writes the content into the directory specified

---
+++ putZipWrite/README.md
@@ -0,0 +1,9 @@
+Accepts a zip file in put request and writes the content into the directory specified.
+
+To get server up and running. Do the following in order.
+
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py runserver
+
+If everything goes well, you can see server running at http://127.0.0.1:8000/
---
+++ zipapp/views.py
@@ -0,0 +1,14 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import zipfile
+
+class PutFileRESTView(APIView):
+
+    def put(self, request, *args, **kwargs):
+        zip_file = request.PUT('zip_file')
+        zip_ref = zipfile.ZipFile(zip_file, 'r')
+        zip_ref.extractall('directory_to_extract_to')
+        zip_ref.close()
+        response = Response('Extracted and wrote zip file to the specified direc', status=status.HTTP_200_OK)
+        return response
